518b9af61ed36d67755304ab3feff3f07d4fb13d
4nDr3ss1 <andreshernanmejia@gmail.com>
2024-05-27 02:45:00 -0400

    Version27-05-24

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+import string
+import random
 #from flask_login import LoginM logout_user, loginmanager, login_user,n_required, current_user, UserMixin
 
 # Crear la aplicación Flask
@@ -21,6 +24,8 @@ class Clase(conexion.Model):
     ID_clase = conexion.Column(conexion.Integer, primary_key=True, autoincrement=True)
     nombre_clase = conexion.Column(conexion.String(40))
     color_clase = conexion.Column(conexion.String(30))
+    fondo_clase = conexion.Column(conexion.String(150))
+    codigo_clase = conexion.Column(conexion.String(10))
     ID_usuario = conexion.Column(conexion.Integer, conexion.ForeignKey('usuario.ID_usuario'))
     usuario = conexion.relationship("Usuario")
 
@@ -35,16 +40,25 @@ class Flashcard(conexion.Model):
     pregunta = conexion.Column(conexion.String(200))
     respuesta = conexion.Column(conexion.String(200))
     ID_tema = conexion.Column(conexion.Integer, conexion.ForeignKey('tema.ID_tema'))
+    calificacion = conexion.Column(conexion.Integer)
+    dificultad = conexion.Column(conexion.String(20))
+    comentario = conexion.Column(conexion.String(200))
+    estado = conexion.Column(conexion.String(30))
     clase = conexion.relationship("Tema")
     
-class UsuarioClase(conexion.Model):
+class Inscripcion(conexion.Model):
     ID_usuario_clase = conexion.Column(conexion.Integer, primary_key=True, autoincrement=True)
     ID_usuario = conexion.Column(conexion.Integer, conexion.ForeignKey('usuario.ID_usuario'))
     ID_clase = conexion.Column(conexion.Integer, conexion.ForeignKey('clase.ID_clase'))
-    codigo = conexion.Column(conexion.String(10))
     usuario = conexion.relationship("Usuario")
     clase = conexion.relationship("Clase")
     
+class Registro(conexion.Model):
+    ID_registro = conexion.Column(conexion.Integer, primary_key=True)
+    conteo_revision_f = conexion.Column(conexion.Integer, nullable=True)
+    conteo_compartido_f = conexion.Column(conexion.Integer, nullable=True)
+    ID_usuario = conexion.Column(conexion.Integer, conexion.ForeignKey('usuario.ID_usuario'))
+    usuario = conexion.relationship("Usuario")
 # Definir rutas y vistas
 @app.route('/')
 def index():
@@ -71,7 +85,7 @@ def login():
             if logged_user.rol == 'estudiante':
              return redirect(url_for('clases'))
             if logged_user.rol == 'docente':
-             return redirect(url_for('login'))
+             return redirect(url_for('clases'))
         else:
             flash("No está registrado")
             return redirect(url_for('vista_login'))
@@ -106,23 +120,39 @@ def cambiar_estado_Docente():
     estado_variable = 'docente'
 
     return render_template('register.html')
-
+# /clases sirve para DOCENTE y ESTUDIANTE
 @app.route('/clases')
 def clases():
     global clases_filtradas
-    clases_filtradas = Clase.query.filter_by(ID_usuario=logged_user.ID_usuario) #Join
-    if clases_filtradas:
-        return render_template('/panel-clases.html', clases=clases_filtradas)
-        
+    clases_filtradas = conexion.session.query(Clase).join(Inscripcion).filter(Inscripcion.ID_usuario == logged_user.ID_usuario).all()
+    if logged_user.rol == 'estudiante':
+        return render_template('/estudiante/e-panel-clases.html', clases=clases_filtradas)
+    if logged_user.rol == 'docente':
+        return render_template('/docente/d-panel-clases.html', clases=clases_filtradas)
+
+@app.route('/clases/unirse', methods=['POST', 'GET'])
+def unirse():
+    if request.method == 'POST':
+        codigo_ingresado = request.form['codigo']
+        codigo_filtrado = Clase.query.filter_by(codigo_clase=codigo_ingresado).first()
+        if codigo_filtrado:
+            nueva_inscripcion = Inscripcion(ID_usuario=logged_user.ID_usuario, ID_clase=codigo_filtrado.ID_clase)
+            conexion.session.add(nueva_inscripcion)
+            conexion.session.commit()
+        ##else agregar mensaje flash
+            
+            return redirect('/clases')
+    return redirect('/clases')      
+    
 @app.route('/temas/<idClase>')
 def temas(idClase):
     global clase_seleccionada
     clase_seleccionada = Clase.query.filter_by(ID_clase=idClase).first()
-    temas_filtrados = Tema.query.filter_by(ID_clase=clase_seleccionada.ID_clase)
-    if temas_filtrados:
-        return render_template('panel-temas.html', temas=temas_filtrados, clases = clases_filtradas, clase_seleccionada=clase_seleccionada)
-    else:
-        return render_template('register.html') #Manejo de errores
+    temas_filtrados = Tema.query.filter_by(ID_clase=clase_seleccionada.ID_clase) ##Cambiar la BD en esta parte
+    if temas_filtrados and logged_user.rol == 'estudiante':
+        return render_template('/estudiante/e-panel-temas.html', temas=temas_filtrados, clases = clases_filtradas, clase_seleccionada=clase_seleccionada)
+    if temas_filtrados and logged_user.rol == 'docente':
+        return render_template('/docente/d-panel-temas.html', temas=temas_filtrados, clases = clases_filtradas, clase_seleccionada=clase_seleccionada) #Manejo de errores
 
 @app.route('/flash/<idTema>')
 def flash(idTema):
@@ -131,19 +161,41 @@ def flash(idTema):
     global tema_seleccionado
     tema_seleccionado = Tema.query.filter_by(ID_tema=idTema).first()
     flash_filtradas = Flashcard.query.filter_by(ID_tema=tema_seleccionado.ID_tema) ##Hay que cambiar
-    if flash_filtradas:
-        return render_template('panel-flashcards.html', flashcards=flash_filtradas, clases = clases_filtradas, mostrar = mostrar, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
-    else:
-        return render_template('register.html') #Manejo de errores
+    if flash_filtradas and logged_user.rol == 'estudiante':
+        return render_template('/estudiante/e-panel-flashcards.html', flashcards=flash_filtradas, clases = clases_filtradas, mostrar = mostrar, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
+    if flash_filtradas and logged_user.rol == 'docente':
+        return render_template('/docente/d-panel-flashcards.html', flashcards=flash_filtradas, clases = clases_filtradas, mostrar = mostrar, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
     
 @app.route('/flashcard/<idFlash>')
 def flashcard(idFlash):
     mostrar =True
+    global flashcard_seleccionada
     flashcard_seleccionada = Flashcard.query.filter_by(ID_flashcard=idFlash).first()
-    if flashcard_seleccionada:
-        return render_template('panel-flashcards.html', flashcard_seleccionada=flashcard_seleccionada, clases = clases_filtradas, mostrar = mostrar, flashcards=flash_filtradas, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
-    else:
-        return render_template('register.html') #Manejo de errores
+    if flashcard_seleccionada and logged_user.rol == 'estudiante':
+        return render_template('estudiante/e-panel-flashcards.html', flashcard_seleccionada=flashcard_seleccionada, clases = clases_filtradas, mostrar = mostrar, flashcards=flash_filtradas, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
+    if flashcard_seleccionada and logged_user.rol == 'docente':
+        return render_template('docente/d-panel-flashcards.html', flashcard_seleccionada=flashcard_seleccionada, clases = clases_filtradas, mostrar = mostrar, flashcards=flash_filtradas, clase_seleccionada=clase_seleccionada, tema_seleccionado=tema_seleccionado)
+    return render_template('register.html') #Manejo de errores
+
+@app.route('/cambiar_dificultad/<idFlash>/<dificultad>')
+def seleccionar_dificultad(idFlash, dificultad):
+    url = '/flashcard/{}'.format(idFlash)
+    conexion.session.query(Flashcard).filter_by(ID_flashcard=flashcard_seleccionada.ID_flashcard).update({"dificultad": dificultad})
+    conexion.session.commit()
+    return redirect(url)
+   
+@app.route('/enviar_revision/<idFlash>')
+def enviar_revision(idFlash):
+    url = '/flashcard/{}'.format(idFlash)
+    if (flashcard_seleccionada.estado != 'compartido'):
+        if(flashcard_seleccionada.estado == 'privado'):
+            conexion.session.query(Flashcard).filter_by(ID_flashcard=flashcard_seleccionada.ID_flashcard).update({"estado": 'revision'})
+            conexion.session.commit()
+        else:
+            conexion.session.query(Flashcard).filter_by(ID_flashcard=flashcard_seleccionada.ID_flashcard).update({"estado": 'privado'})
+            conexion.session.commit()
+        
+    return redirect(url)
 
 @app.route('/flashcard/crear', methods=['POST'])
 def crearFlash():
@@ -152,7 +204,7 @@ def crearFlash():
         nueva_pregunta = request.form['pregunta']
         nueva_respuesta = request.form['respuesta']
         
-        nueva_flashcard = Flashcard(pregunta=nueva_pregunta, respuesta=nueva_respuesta, ID_tema=tema_seleccionado.ID_tema)
+        nueva_flashcard = Flashcard(pregunta=nueva_pregunta, respuesta=nueva_respuesta, ID_tema=tema_seleccionado.ID_tema, estado='privado')
         conexion.session.add(nueva_flashcard)
         conexion.session.commit()
         
@@ -161,7 +213,55 @@ def crearFlash():
     
     ####ROL DOCENTE
     
+@app.route('/clases/crear', methods=['POST'])
+def crearClase():
+   # url = '/flash/{}'.format(tema_seleccionado.ID_tema)
+    if request.method == 'POST':
+        nuevo_titulo = request.form['titulo']
+        nuevo_fondo = request.form['fondo']
+        nuevo_color = request.form['color']
+        
+        nueva_clase = Clase(nombre_clase=nuevo_titulo, color_clase=nuevo_color, fondo_clase=nuevo_fondo, ID_usuario=logged_user.ID_usuario, codigo_clase=generarCodigo())
+        conexion.session.add(nueva_clase)
+        conexion.session.commit()
+        nuevo_usuario_clase = Inscripcion(ID_usuario=logged_user.ID_usuario, ID_clase=nueva_clase.ID_clase)
+        conexion.session.add(nuevo_usuario_clase)
+        conexion.session.commit()
+            
+        return redirect('/clases')
+    return redirect('/clases')  
+
+@app.route('/temas/crear', methods=['POST'])
+def crearTema():
+   # url = '/flash/{}'.format(tema_seleccionado.ID_tema)
+    if request.method == 'POST':
+        nuevo_titulo = request.form['titulo']
+        
+        nuevo_tema = Tema(nombre_tema=nuevo_titulo, ID_clase=clase_seleccionada.ID_clase)
+        conexion.session.add(nuevo_tema)
+        conexion.session.commit()
+        
+        return redirect(f'/temas/{clase_seleccionada.ID_clase}')
+    return redirect(f'/temas/{clase_seleccionada.ID_clase}') 
+
+@app.route('/lista_estudiantes')
+def lista_estudiantes():
     
+    estudiantes_filtrados = conexion.session.query(Usuario).join(Inscripcion).filter(Usuario.rol == 'estudiante').all()
+   
+    return render_template('/docente/d-lista-estudiantes.html', clases = clases_filtradas, clase_seleccionada=clase_seleccionada, estudiantes=estudiantes_filtrados)
+
+
+
+
+def generarCodigo():
+    caracteres = string.ascii_uppercase + string.digits  
+    codigo_generado = ''.join(random.choice(caracteres) for _ in range(6))
+    codigos_filtrados = Clase.query.filter_by(codigo_clase=codigo_generado).all()
+    if codigos_filtrados:
+        codigo_generado = generarCodigo()
+    else:
+        return codigo_generado
     
 # Punto de entrada para ejecutar la aplicación
 if __name__ == '__main__':
